ae46b44b51dc843c20f63c8800846dcf250b72c5
Maharshi <61038992+Manohar-Maharshi@users.noreply.github.com>
2021-08-21 13:00:28 +0530

    Delete src directory

--- src/components/SearchHeader.svelte
+++
@@ -1,7 +0,0 @@
-<div class="float-right py-4">
-	<div class="w-full flex items-center space-x-5 text-sm">
-		<a href="/" class="hover:underline">Gmail</a>
-		<a href="/" class="hover:underline">Images</a>
-		<a href="/" class="h-10 w-10 bg-blue-200 rounded-full"></a>
-	</div>
-</div>
